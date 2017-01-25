fc3aec08968ab2e430d371fa0b1ba95b3e09d1de
mukeshv483 <mukeshv483@outlook.com>
2017-01-26 04:42:58 +0530

    update

--- src/test/java/myproject/myproject_12/AppTest.java
+++ src/test/java/myproject/myproject_12/AppTest.java
@@ -33,7 +33,21 @@ public class AppTest
      */
     public void testApp()
     {
-    	
+    	System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+       int i,j,k;
+for(i=1; i<=5; i++)
+{
+for(j=4; j>=i; j--)
+{
+System.out.print(" ");
+}
+for(k=1; k<=(2*i-1); k++)
+{
+System.out.print("*");
+}
+System.out.println("");
+}
        
         System.out.println( "Hello World!" );
    	 System.out.println( "Hello World!" );
