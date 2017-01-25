f3c67a1b21a3ad870df4d5a5c525863afe87bdf8
mukeshv483 <mukeshv483@outlook.com>
2017-01-26 04:33:24 +0530

    update

--- src/test/java/myproject/myproject_12/AppTest.java
+++ src/test/java/myproject/myproject_12/AppTest.java
@@ -33,8 +33,7 @@ public class AppTest
      */
     public void testApp()
     {
-    	System.out.println( "Hello me" );
-        System.out.println( "Hello me" );
+    	
        
         System.out.println( "Hello World!" );
    	 System.out.println( "Hello World!" );
